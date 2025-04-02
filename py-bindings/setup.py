c5e66094b6edb44cb3a0e43d39dd847e090f30fa
Michael Ripperger <michael.ripperger@swri.org>
2025-04-01 21:55:58 -0500

    Updated Python package name

--- py-bindings/setup.py
+++ py-bindings/setup.py
@@ -149,7 +149,7 @@ class CMakeBuild(build_ext):
 
 
 setup(
-    name="ompl",
+    name="swri_ompl",
     version="1.6.0",
     description="The Open Motion Planning Library",
     author="Ioan A. Șucan, Mark Moll, Zachary Kingston, Lydia E. Kavraki",
