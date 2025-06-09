8fb6554259e0cc088ab95e1b5a2661f1095c79b5
Michael Ripperger <michael.ripperger@swri.org>
2025-06-09 15:35:20 -0500

    Tighten specs on dependencies

--- .github/workflows/before_all.sh
+++ .github/workflows/before_all.sh
@@ -6,10 +6,10 @@ build_os="$(uname)"
 
 if [ "${build_os}" == "Linux" ]; then
     yum -y install \
-        sudo \
-        eigen3 \
-        llvm-devel \
-        clang-devel
+        sudo-1.9.* \
+        eigen3-devel-3.3.* \
+        llvm-devel-18.1.* \
+        clang-devel-18.1.*
 
     # manylinux ships with a pypy installation. Make it available on the $PATH
     # so the OMPL build process picks it up and can make use of it during the
